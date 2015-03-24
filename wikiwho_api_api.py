a891f6cd3596c1cb8d957a41464932fd8815cbed
psinger <killver@gmail.com>
2015-03-24 17:06:21 +0100

    minor

--- wikiwho_api_api.py
+++ wikiwho_api_api.py
@@ -106,81 +106,26 @@ if __name__ == '__main__':
         if rvcontinue != 0:
             params['rvcontinue'] = rvcontinue
 
-        result = requests.get(url=url, headers=headers, params=params).json()
+        try:
+            result = requests.get(url=url, headers=headers, params=params).json()
+        except:
+            Wikiwho.printFail(reviid, message="HTTP Response error! Try again later!")
 
         if 'error' in result: Wikiwho.printFail(reviid, message="Wikipedia API returned the following error:" + result['error'])
         if 'warnings' in result: Wikiwho.printFail(reviid, message="Wikipedia API returned the following warning:" + result['warning'])
         if 'query' in result:
-            wikiwho.analyseArticle(result['query']['pages'].itervalues().next()['revisions'])
+            try:
+                if "-1" in result['query']['pages']:
+                    Wikiwho.printFail(reviid, message="The article you are trying to request does not exist!")
+                wikiwho.analyseArticle(result['query']['pages'].itervalues().next()['revisions'])
+            except:
+                Wikiwho.printFail(reviid, message="Some problems with the returned XML by Wikipedia!")
         if 'continue' not in result: break
         rvcontinue = result['continue']['rvcontinue']
         wikiwho.rvcontinue = rvcontinue
         #print rvcontinue
 
-# 	i += 1
-#         if rev_timestamp > timestamp:
-#             logging.debug("NEED TO DO SOMETHING")
-#             #logging.debug(type(rev_timestamp))
-#             #logging.debug(type(timestamp))
-#             logging.debug("wikiwho timestamp first")
-#             logging.debug(timestamp)
-#             if timestamp.year == 1900:
-#
-# params = {'titles':'Gamergate_controversy', 'action':'query', 'prop':'revisions', 'rvprop':'content|ids', 'rvlimit':'max', 'format':'json'}
-#
-#
-#
-#                 url ='http://en.wikipedia.org/w/index.php'
-#                 enc = {'title':'Special:Export', 'pages':art, 'action':'submit'}
-#             else:
-#                 logging.debug('doing partial download')
-#                 url ='http://en.wikipedia.org/w/index.php'
-#                 enc = {'title':'Special:Export','pages':art,'offset':timestamp,'action':'submit'}
-#             # print timestamp
-#             # #	enc = {'pages':art,'limit':'47','action':'submit'}
-#             try:
-#                 #data = urllib.urlencode(enc)
-#                 #req = urllib2.Request(url=url,data=data)
-#                 #response = urllib2.urlopen(req)
-#                 r = requests.post(url, params=enc, stream=True)
-#             except:
-# 		#print str(e)
-#                 Wikiwho.printFail(reviid, message="HTTP Response error! Try again later!")
-#         # response = open("Finger_Lakes3_until4.xml")
-#         # reviid = 27
-#             #sys.exit()
-# 	    #try:
-#             #if timestamp <= wikiwho.lastrev_date:
-#             #    Wikiwho.printFail(reviid, message="Something went awfully wrong with the timestamps!")
-#             text = r.content
-#
-#             text_file = open("test.xml", "w+")
-#             text_file.write(text)
-#             text_file.close()
-#
-#             try:
-#                 wikiwho.analyseArticle(StringIO(text))
-#             except:
-#                 logging.debug("ouch, Wikipedia returns bullshit!")
-#                 logging.debug("pickling")
-#                 f = io.open("pickle/" + art + ".p",'wb')
-#                 cPickle.dump(wikiwho, f, protocol =-1)
-#                 Wikiwho.printFail(reviid, message="Some problems with the returned XML by Wikipedia! Parsing error! Please try again!")
-#             #wikiwho.analyseArticle(StringIO(response.read()))
-#             #except:
-# 	#	Wikiwho.printFail(reviid, message="Some problems with the returned XML by Wikipedia! Parsing error!")
-#
-#
-#             timestamp = wikiwho.lastrev_date
-#             logging.debug("wikiwho timestamp after")
-#             logging.debug(timestamp)
-#         else:
-#             logging.debug("NO NEED TO DO ANYTHING")
-#             break
-#
-#     logging.debug(wikiwho.lastrev)
-#     logging.debug(wikiwho.lastrev_date)
-#
+
 
     #print len(wikiwho.revisions)
 
