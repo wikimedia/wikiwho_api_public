b66bfec79aa85665cf3eddc895cf3b86bbdb41c3
Fabian Flöck <f.floeck@gmail.com>
2016-05-23 11:58:49 +0200

    login fixed, should work now (test script for login only)

--- wp_login.py
+++ wp_login.py
@@ -5,22 +5,37 @@ __author__ = 'ffl'
 # Tested with MediaWiki 1.22.7
 
 import requests
+import json
+import urllib
 
-user    = 'Fabian%20Fl%C3%B6ck'
-passw   = 'rumkugeln'
-authu   = 'apacheuser'    # For Apache Basic Auth
-authp   = 'apachepass'    # For Apache Basic Auth
-baseurl = 'https://en.wikipedia.org/w/'
-params  = '?action=login&lgname=%s&lgpassword=%s&format=json'% (user,passw)
+url ='https://en.wikipedia.org/w/api.php'
+
+headers = {
+    'User-Agent': 'Wikiwho API',
+    'From': 'philipp.singer@gesis.org and fabian.floeck@gesis.org'
+}
+
+
+session = requests.session()
+
+user    = 'Fabian%20Fl%C3%B6ck@wikiwho'
+passw   = 'o2l009t25ddtlefdt6cboctj8hk8nbfs'
+
+params  = '?action=query&meta=tokens&type=login&format=json'
 
 # Login request
-r1 = requests.post(baseurl+'api.php'+params,auth=(authu,authp))
-token = r1.json()['login']['token']
-params2 = params+'&lgtoken=%s'% token
+r1 = session.post(url+params)
+#print r1.json()
+#r1 = json.loads(r1)
+token = r1.json()["query"]["tokens"]["logintoken"]
+token = urllib.quote(token)
 
+print 'token', token
+params2 = '?action=login&lgname=%s&lgpassword=%s&lgtoken=%s&format=json'% (user,passw,token)
+#print 'params2', params2
 # Confirm token; should give "Success"
-r2 = requests.post(baseurl+'api.php'+params2,auth=(authu,authp),cookies=r1.cookies)
-print r2.json()['login']['result']
+r2 = session.post(url+params2)
+print r2.json()
 
 # Try accessing a private MediaWiki page
 #r3 = requests.get(baseurl+'index.php/PrivateTest',
