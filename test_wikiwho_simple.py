3d97d480179131bc991b0a0592991d666f23ee96
Kenan Erdogan <kenanerdogan@gmail.com>
2016-07-21 15:36:44 +0200

    test_json_output method

--- test_wikiwho_simple.py
+++ test_wikiwho_simple.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+To run only test_authorship test calls:
+py.test test_wikiwho_simple.py::TestWikiwho::test_authorship
+To run only test_json_output test calls:
+py.test test_wikiwho_simple.py::TestWikiwho::test_json_output
+To run tests on specific lines of input excel:
+py.test test_wikiwho_simple.py --lines=3,9
+py.test test_wikiwho_simple.py::TestWikiwho::test_authorship --lines=3,9
+"""
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from builtins import range
@@ -6,6 +15,9 @@ from openpyxl import load_workbook
 import os
 import sys
 import pytest
+import json
+import filecmp
+import io
 
 from handler import WPHandler
 from structures.Text import splitIntoWords
@@ -20,8 +32,10 @@ def pytest_generate_tests(metafunc):
     """
     # print(metafunc)  # http://docs.pytest.org/en/latest/parametrize.html#the-metafunc-object
     # print(metafunc.config.option.lines)
+    articles = set()
     lines = metafunc.config.option.lines
     lines = [] if lines == 'all' else [int(l) for l in lines.split(',')]
+    # file_or_dir = metafunc.config.option.file_or_dir  # TODO
     if "article_name" in metafunc.fixturenames:
         wb = load_workbook(filename='test_wikiwho_simple.xlsx', data_only=True, read_only=True)
         ws = wb[wb.sheetnames[0]]
@@ -30,14 +44,23 @@ def pytest_generate_tests(metafunc):
                 continue
             if not row[0].value:
                 break
-            funcargs = {
-                'article_name': row[0].value,
-                'revision_ids': [int(row[2].value)],
-                'token': '{}'.format(row[3].value).lower(),
-                'context': '{}'.format(row[4].value).lower(),
-                'correct_rev_id': int(row[5].value),
-            }
-            metafunc.addcall(funcargs=funcargs)
+            article_name = row[0].value.replace(' ', '_')
+            if 'token' in metafunc.fixturenames:
+                funcargs = {
+                    'article_name': article_name,
+                    'revision_ids': [int(row[2].value)],
+                    'token': '{}'.format(row[3].value).lower(),
+                    'context': '{}'.format(row[4].value).lower(),
+                    'correct_rev_id': int(row[5].value),
+                }
+                metafunc.addcall(funcargs=funcargs)
+            elif article_name not in articles:
+                articles.add(article_name)
+                funcargs = {
+                    'article_name': article_name,
+                    'revision_ids': [int(row[2].value)],
+                }
+                metafunc.addcall(funcargs=funcargs)
 
 
 class TestWikiwho:
@@ -85,6 +108,17 @@ class TestWikiwho:
                                                                                                  correct_rev_id)
                 break
         assert found, "{}: {} -> token not found".format(article_name, token)
+
+    def test_json_output(self, temp_folder, article_name, revision_ids):
+        with WPHandler(article_name, temp_folder) as wp:
+            wp.handle(revision_ids, 'json')
+        response = wp.wikiwho.print_revision(wp.revision_ids, {}, return_response=True)
+        json_file_path = '{}/{}.json'.format(temp_folder, article_name)
+        test_json_file_path = 'test_jsons/{}.json'.format(article_name)
+        with io.open(json_file_path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(response, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False))
+        is_content_same = filecmp.cmp(json_file_path, test_json_file_path)
+        assert is_content_same
 
     @classmethod
     def teardown_class(cls):
