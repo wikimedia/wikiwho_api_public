6cef4d7682d68b8a986bacb7652ee65b350cf1a9
Kenan Erdogan <kenanerdogan@gmail.com>
2016-11-04 13:28:49 +0100

    update settings

--- wikiwho_api/settings_staging.py
+++ wikiwho_api/settings_staging.py
@@ -3,7 +3,17 @@ from .settings_base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+INSTALLED_APPS += ['debug_toolbar']
 SWAGGER_SETTINGS['VALIDATOR_URL'] = 'https://online.swagger.io/validator'
 
-# ALLOWED_HOSTS = ['www.wikiwho.net',
-#                  'wikiwho.net']
+ALLOWED_HOSTS = ['193.175.238.88',
+                 'www.wikiwho.net',
+                 'wikiwho.net']
+
+
+def custom_show_toolbar(request):
+    return True  # show toolbar always for staging
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+}
