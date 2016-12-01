73bcc5054c754db13b303b461e823f7dddd76b5c
Kenan Erdogan <kenanerdogan@gmail.com>
2016-12-01 10:37:04 +0100

    tests for wp xml dumps

--- wikiwho/tests/test_wikiwho_simple.py
+++ wikiwho/tests/test_wikiwho_simple.py
@@ -131,32 +131,29 @@ class TestWikiwho:
         v = WikiwhoApiView()
         v.article = wp.article_obj
 
-        # create json without token ids
+        # create json with rev and author ids
         revision_json_without_tokenid = v.get_revision_json(wp.revision_ids, {'rev_id', 'author'})
         json_file_path_without_tokenid = '{}/{}_db_ri_ai.json'.format(temp_folder, article_name)
         with io.open(json_file_path_without_tokenid, 'w', encoding='utf-8') as f:
             f.write(json.dumps(revision_json_without_tokenid, indent=4, separators=(',', ': '),
                                sort_keys=True, ensure_ascii=False))
-
-        # compare jsons without token ids
+        # compare jsons with rev and author ids
         test_json_file_path = '{}/{}_db_ri_ai.json'.format(test_json_folder, article_name)
-        is_content_same = filecmp.cmp(json_file_path_without_tokenid, test_json_file_path)
-        assert is_content_same, "{}: 'json without token ids' doesn't match".format(article_name)
+        is_content_same_1 = filecmp.cmp(json_file_path_without_tokenid, test_json_file_path)
 
-        # check if all token ids are unique
+        # create json with token ids
         revision_json = v.get_revision_json(wp.revision_ids, {'token_id'})
+        # check if all token ids are unique
         for _, rev in revision_json['revisions'][0].items():
             token_ids = [t['token_id'] for t in rev['tokens']]
             assert len(token_ids) == len(set(token_ids)), "{}: there are duplicated token ids".format(article_name)
             break
-
         # compare jsons with token ids
         json_file_path = '{}/{}_db_ti.json'.format(temp_folder, article_name)
         test_json_file_path = '{}/{}_db_ti.json'.format(test_json_folder, article_name)
         with io.open(json_file_path, 'w', encoding='utf-8') as f:
             f.write(json.dumps(revision_json, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False))
-        is_content_same = filecmp.cmp(json_file_path, test_json_file_path)
-        assert is_content_same, "{}: json doesn't match".format(article_name)
+        is_content_same_2 = filecmp.cmp(json_file_path, test_json_file_path)
 
         # create json with in/outbounds
         revision_json_with_io = v.get_revision_json(wp.revision_ids, {'inbound', 'outbound'})
@@ -164,11 +161,83 @@ class TestWikiwho:
         with io.open(json_file_path_with_io, 'w', encoding='utf-8') as f:
             f.write(json.dumps(revision_json_with_io, indent=4, separators=(',', ': '),
                                sort_keys=True, ensure_ascii=False))
-
         # compare jsons with in/outbounds
         test_json_file_path = '{}/{}_db_io.json'.format(test_json_folder, article_name)
-        is_content_same = filecmp.cmp(json_file_path_with_io, test_json_file_path)
-        assert is_content_same, "{}: 'json with in/outbounds' doesn't match".format(article_name)
+        is_content_same_3 = filecmp.cmp(json_file_path_with_io, test_json_file_path)
+
+        assert is_content_same_1, "{}: 'json with ri and ai doesn't match".format(article_name)
+        assert is_content_same_2, "{}: json with ti doesn't match".format(article_name)
+        assert is_content_same_3, "{}: 'json with in/outbounds doesn't match".format(article_name)
+
+    def test_json_output_xml(self, temp_folder, article_name, revision_ids):
+        """
+        Tests json outputs of articles in given revisions in gold standard from xml dump.
+        If there are expected differences in jsons, authorship of each token in gold standard should be
+        checked manually.
+        The xml file dumps taken from here: https://dumps.wikimedia.org/enwiki/20161101/
+        :param revision_ids: Revision id where authorship of token in gold standard is tested.
+        """
+        temp_folder = '{}/test_json_output_xml'.format(temp_folder)
+        test_json_folder = 'test_jsons'
+
+        from mwxml import Dump
+        from mwtypes.files import reader
+        # faster: read from gold standard articles xml
+        # xml_file_path = 'test_jsons/gold_standard_articles_20161101.xml'
+        # slower: read from xml dumps (7z)
+        xml_file_path = 'test_jsons/{}'.format(article_zips[article_name])
+        dump = Dump.from_file(reader(xml_file_path))
+        article_name = article_name.replace(' ', '_')
+        title_matched = False
+        for page in dump:
+            if page.title.replace(' ', '_') == article_name:
+                title_matched = True
+                with WPHandler(article_name, temp_folder) as wp:
+                    wp.handle_from_xml(page)
+                break
+
+        assert title_matched, '{}--{}'.format(article_name, page.title)
+
+        v = WikiwhoApiView()
+        v.article = wp.article_obj
+
+        # create json with rev and author ids
+        revision_json_without_tokenid = v.get_revision_json(revision_ids, {'rev_id', 'author'})
+        json_file_path_without_tokenid = '{}/{}_db_ri_ai.json'.format(temp_folder, article_name)
+        with io.open(json_file_path_without_tokenid, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(revision_json_without_tokenid, indent=4, separators=(',', ': '),
+                               sort_keys=True, ensure_ascii=False))
+        # compare jsons with rev and author ids
+        test_json_file_path = '{}/{}_db_ri_ai.json'.format(test_json_folder, article_name)
+        is_content_same_1 = filecmp.cmp(json_file_path_without_tokenid, test_json_file_path)
+
+        # create json with token ids
+        revision_json = v.get_revision_json(revision_ids, {'token_id'})
+        # check if all token ids are unique
+        for _, rev in revision_json['revisions'][0].items():
+            token_ids = [t['token_id'] for t in rev['tokens']]
+            assert len(token_ids) == len(set(token_ids)), "{}: there are duplicated token ids".format(article_name)
+            break
+        # compare jsons with token ids
+        json_file_path = '{}/{}_db_ti.json'.format(temp_folder, article_name)
+        test_json_file_path = '{}/{}_db_ti.json'.format(test_json_folder, article_name)
+        with io.open(json_file_path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(revision_json, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False))
+        is_content_same_2 = filecmp.cmp(json_file_path, test_json_file_path)
+
+        # create json with in/outbounds
+        revision_json_with_io = v.get_revision_json(revision_ids, {'inbound', 'outbound'})
+        json_file_path_with_io = '{}/{}_db_io.json'.format(temp_folder, article_name)
+        with io.open(json_file_path_with_io, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(revision_json_with_io, indent=4, separators=(',', ': '),
+                               sort_keys=True, ensure_ascii=False))
+        # compare jsons with in/outbounds
+        # test_json_file_path = '{}/{}_db_io.json'.format(test_json_folder, article_name)
+        # is_content_same_3 = filecmp.cmp(json_file_path_with_io, test_json_file_path)
+
+        assert is_content_same_1, "{}: 'json with ri and ai doesn't match".format(article_name)
+        assert is_content_same_2, "{}: json with ti doesn't match".format(article_name)
+        # assert is_content_same_3, "{}: 'json with in/outbounds doesn't match".format(article_name)
 
     def test_continue_logic(self, temp_folder, article_name, revision_id_start, revision_id_end):
         """
@@ -367,3 +436,98 @@ class TestWikiwho:
         #     tmp = 'tmp_test'
         # if os.path.exists(tmp):
         #     os.removedirs(tmp)
+
+
+article_zips = {
+    'Amstrad_CPC': 'enwiki-20161101-pages-meta-history1.xml-p000000010p000002289.7z',
+    'Antarctica': 'enwiki-20161101-pages-meta-history20.xml-p018754736p018984527.7z',
+    'Apollo_11': 'enwiki-20161101-pages-meta-history1.xml-p000000010p000002289.7z',
+    'Armenian_Genocide': 'enwiki-20161101-pages-meta-history3.xml-p000118475p000143283.7z',
+    'Barack_Obama': 'enwiki-20161101-pages-meta-history5.xml-p000518010p000549136.7z',
+    'Bioglass': 'enwiki-20161101-pages-meta-history9.xml-p002071291p002171781.7z',
+    'Bothrops_jararaca': 'enwiki-20161101-pages-meta-history15.xml-p007991091p008292517.7z',
+    'Chlorine': 'enwiki-20161101-pages-meta-history1.xml-p000004536p000006546.7z',
+    'Circumcision': 'enwiki-20161101-pages-meta-history15.xml-p008592059p008821460.7z',
+    'Communist_Party_of_China': 'enwiki-20161101-pages-meta-history1.xml-p000006547p000008653.7z',
+    'Democritus': 'enwiki-20161101-pages-meta-history1.xml-p000006547p000008653.7z',
+    'Diana,_Princess_of_Wales': 'enwiki-20161101-pages-meta-history1.xml-p000022917p000025445.7z',
+    'Encryption': 'enwiki-20161101-pages-meta-history1.xml-p000008654p000010882.7z',
+    'Eritrean_Defence_Forces': 'enwiki-20161101-pages-meta-history1.xml-p000008654p000010882.7z',
+    'European_Free_Trade_Association': 'enwiki-20161101-pages-meta-history1.xml-p000008654p000010882.7z',
+    'Evolution': 'enwiki-20161101-pages-meta-history1.xml-p000008654p000010882.7z',
+    'Geography_of_El_Salvador': 'enwiki-20161101-pages-meta-history1.xml-p000008654p000010882.7z',
+    'Germany': 'enwiki-20161101-pages-meta-history1.xml-p000010883p000013026.7z',
+    'Home_and_Away': 'enwiki-20161101-pages-meta-history3.xml-p000161222p000169747.7z',
+    'Homeopathy': 'enwiki-20161101-pages-meta-history1.xml-p000013027p000015513.7z',
+    'Iraq_War': 'enwiki-20161101-pages-meta-history13.xml-p005040438p005137507.7z',
+    'Islamophobia': 'enwiki-20161101-pages-meta-history3.xml-p000161222p000169747.7z',
+    'Jack_the_Ripper': 'enwiki-20161101-pages-meta-history14.xml-p006733138p006933850.7z',
+    'Jesus': 'enwiki-20161101-pages-meta-history7.xml-p001063241p001127973.7z',
+    'KLM_destinations': 'enwiki-20161101-pages-meta-history9.xml-p002071291p002171781.7z',
+    'Lemur': 'enwiki-20161101-pages-meta-history5.xml-p000466359p000489651.7z',
+    'Macedonians_(ethnic_group)': 'enwiki-20161101-pages-meta-history5.xml-p000420318p000440017.7z',
+    'Muhammad': 'enwiki-20161101-pages-meta-history1.xml-p000017892p000020545.7z',
+    'Newberg,_Oregon': 'enwiki-20161101-pages-meta-history3.xml-p000118475p000143283.7z',
+    'Race_and_intelligence': 'enwiki-20161101-pages-meta-history1.xml-p000025446p000028258.7z',
+    'Rhapsody_on_a_Theme_of_Paganini': 'enwiki-20161101-pages-meta-history4.xml-p000215173p000232405.7z',
+    'Robert_Hues': 'enwiki-20161101-pages-meta-history20.xml-p019630121p020023800.7z',
+    "Saturn's_moons_in_fiction": 'enwiki-20161101-pages-meta-history14.xml-p006733138p006933850.7z',
+    'Sergei_Korolev': 'enwiki-20161101-pages-meta-history2.xml-p000078261p000088444.7z',
+    'South_Western_Main_Line': 'enwiki-20161101-pages-meta-history8.xml-p001348476p001442630.7z',
+    'Special_Air_Service': 'enwiki-20161101-pages-meta-history2.xml-p000050799p000057690.7z',
+    'The_Holocaust': 'enwiki-20161101-pages-meta-history16.xml-p010182412p010463377.7z',
+    'Toshitsugu_Takamatsu': 'enwiki-20161101-pages-meta-history9.xml-p002071291p002171781.7z',
+    'Vladimir_Putin': 'enwiki-20161101-pages-meta-history2.xml-p000032259p000034487.7z',
+    'Wernher_von_Braun': 'enwiki-20161101-pages-meta-history2.xml-p000032259p000034487.7z'
+}
+
+
+def create_gold_xml():
+    from collections import defaultdict
+    files = defaultdict(list)
+    for title, file_ in article_zips.items():
+        file_ = 'wikiwho/tests/test_jsons/{}'.format(file_)
+        files[file_].append(title)
+    header = """
+    <mediawiki xmlns="http://www.mediawiki.org/xml/export-0.5/"
+               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.5/
+                 http://www.mediawiki.org/xml/export-0.5.xsd" version="0.5"
+               xml:lang="en">
+    <siteinfo>
+    </siteinfo>
+    """
+    footer = "</mediawiki>"
+    p = list(header)
+
+    import os
+    for xml_file, titles in files.items():
+        # print(os.path.dirname(xml_file))
+        os.system('7z x {} -o{}'.format(xml_file, os.path.dirname(xml_file)))
+        xml_file = xml_file[:-3]
+        p.append('<page>\n')
+        add = False
+        with open(xml_file, 'r') as f:
+            for line in f:
+                if not add:
+                    for title in titles:
+                        if '<title>{}</title>'.format(title) in line or '<title>{}</title>'.format(title.replace(' ', '_')) in line:
+                            add = True
+                            titles.remove(title)
+                if add:
+                    p.append(line)
+                if add and '</page>' in line:
+                    add = False
+                    if not titles:
+                        break
+                    else:
+                        p.append('<page>\n')
+        os.remove(xml_file)
+
+    p.append(footer)
+    with open('gold_standard_articles_20161101.xml', 'a') as f:
+        for line in p:
+            f.write(line)
+
+    print('done')
+    return p
