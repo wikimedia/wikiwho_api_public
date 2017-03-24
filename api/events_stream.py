ce8f0078735b7db184fd45d355759680d74d7130
Kenan Erdogan <kenanerdogan@gmail.com>
2017-03-24 13:32:30 +0100

    wp events

---
+++ api/events_stream.py
@@ -0,0 +1,60 @@
+import json
+import urllib3
+from sseclient import SSEClient
+
+
+class EventSource(SSEClient):
+
+    def _read(self):
+        """Read the incoming event source stream and yield event chunks.
+
+        Unfortunately it is possible for some servers to decide to break an
+        event into multiple HTTP chunks in the response. It is thus necessary
+        to correctly stitch together consecutive response chunks and find the
+        SSE delimiter (empty new line) to yield full, correct event chunks."""
+        data = ''
+        for chunk in self._event_source:
+            for line in chunk.splitlines(True):
+                if not line.strip():
+                    yield data
+                    data = ''
+                # ignore decoding errors, we only need page id and namespace which are numbers and doesn't contain
+                # special chars
+                data += line.decode(self._char_enc, errors='ignore')
+        if data:
+            yield data
+
+
+def stream_response(url):
+    """Get a streaming response for the given event feed using urllib3."""
+    http = urllib3.PoolManager()
+    # the .stream(chunk_size) is important here!  Without it, the
+    # response body seems to be accessed in an inefficient manner.
+    return http.request('GET', url, preload_content=False).stream(1024)
+
+
+def stream_response_with_requests(url):
+    """Get a streaming response for the given event feed using requests."""
+    import requests
+    return requests.get(url, stream=True)
+
+
+def iter_changed_page_ids():
+    # page_ids = []
+    url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+    event_source = stream_response(url)
+    # event_source = stream_response_with_requests(url)
+    for event in EventSource(event_source).events():
+        # print(event.data)  # "id":926316704,"
+        page_id = event.data.split('"id":')[1].split(',"')[0]
+        namespace = event.data.split('"namespace":')[1].split(',"')[0]
+        wiki = event.data.split('"wiki":"')[1].split('"')[0]
+        if wiki == 'enwiki' and namespace == '0' and page_id and page_id != 'null':
+            yield int(page_id)
+            # page_ids.append(page_id)
+            # print(len(page_ids), len(set(page_ids)))
+        # change = json.loads(event.data)
+        # if change['wiki'] == 'enwiki' and change['namespace'] == 0:
+        #     page_id = change['id']
+        #     if page_id:
+        #         yield page_id
