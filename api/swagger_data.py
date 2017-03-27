09c5468b9b461d138a3a128cc5ef057da4d5e991
Kenan Erdogan <kenanerdogan@gmail.com>
2017-03-27 09:43:25 +0200

    update swagger text

--- api/swagger_data.py
+++ api/swagger_data.py
@@ -7,9 +7,9 @@ query_params = [
      'type': 'boolean'},
     {'description': 'Token ID per token', 'in': 'query', 'name': 'token_id', 'required': True,
      'type': 'boolean'},
-    {'description': 'Inbound revision IDs per token', 'in': 'query', 'name': 'in', 'required': True,
-     'type': 'boolean'},
     {'description': 'Outbound revision IDs per token', 'in': 'query', 'name': 'out', 'required': True,
+     'type': 'boolean'},
+    {'description': 'Inbound revision IDs per token', 'in': 'query', 'name': 'in', 'required': True,
      'type': 'boolean'}
 ]
 
@@ -61,7 +61,9 @@ custom_data = {
                            'consists of.\n\n'
                            'For each article page it mirrors its current state on the English Wikipedia.\n\n'
                            'The accuracy is around 96%, evaluated against a gold standard dataset. '
-                           'It\'s based on the [WikiWho algorithm](https://github.com/wikiwho).\n\n'
+                           'It\'s based on the [WikiWho algorithm](https://github.com/wikiwho) - the most accurate '
+                           'algorithm for this task, evaluated against a [gold standard dataset]'
+                           '(http://f-squared.org/wikiwho/#paper) (~95% acc.).\n\n'
                            'Terminology used here:\n\n'
                            '- *Wikipedia*: For now, the English Wikipedia - to be extended in the future.\n'
                            '- *article (page)*: Any Wikipedia page in '
@@ -74,22 +76,31 @@ custom_data = {
                            '`"A [[house]], a boat."` are `"a", "[[", "house", "]]", ",", "a", "boat", "."` '
                            'I.e., all tokens are converted into lower-case and certain character combinations that '
                            'have a specific function in Wiki Markup, such as double-square brackets, get treated '
-                           'as single tokens.\n'
+                           'as single tokens. '
+                           '>> [Current WikiWho tokenization]'
+                           '(https://github.com/wikiwho/WikiWho/blob/master/WikiWho/utils.py)\n'
                            '- *revisions*: The article revisions and their IDs as retrieved from Wikipedia, with one '
                            'exception: The WikiWho algorithm implements a (very lenient)  filter to avoid spending '
                            'time DIFFing blatant vandalism which gets immediately reverted after. About 0.5% of the '
                            'revisions from Wikipedia are hence not available here as we consider those changes to '
-                           'have dissappeared immediately. This is a temporary constraint to be soon removed.\n\n'
+                           'have disappeared immediately. This is a temporary constraint to be removed in an '
+                           'upcoming version.\n\n'
                            '**[>> Toy example for how the token metadata is generated]'
                            '(https://gist.github.com/faflo/8bd212e81e594676f8d002b175b79de8)**\n\n'
                            'See the description of the different query types for more information.\n\n'
                            'A dataset with this data (until Nov. 2016, no redirects) is available for download at '
-                           'https://doi.org/10.5281/zenodo.345571 .'
-                           'Please cite it as well if you use data from this API.\n\n',
-            'contact': {
-                'name': 'GESIS - Leibniz Institute for the Social Sciences',
-                # 'email': 'kenan.erdogan@gesis.org?cc=fabian.floeck@gesis.org&subject=wikiwho API',
-                'url': 'http://www.gesis.org/en/institute/gesis-scientific-departments/computational-social-science/'},
+                           'https://doi.org/10.5281/zenodo.345571.\n\n'
+                           '**Please cite it** as well if you use data from this API in your research.\n\n'
+                           '(note that the dataset excludes redirect articles and tokenization can slightly differ '
+                           'from the API version)\n\n'
+                           '**Created by **[GESIS - Leibniz Institute for the Social Sciences, CSS group]'
+                           '(http://www.gesis.org/en/institute/gesis-scientific-departments/'
+                           'computational-social-science/)',
+            # 'contact': {
+            #     'name': 'GESIS - Leibniz Institute for the Social Sciences, CSS group',
+            #     # 'email': 'kenan.erdogan@gesis.org?cc=fabian.floeck@gesis.org&subject=wikiwho API',
+            #     'url': 'http://www.gesis.org/en/institute/gesis-scientific-departments/'
+            #            'computational-social-science/'},
         },
     'basePath': '/api/v{}'.format(version),
     'host': 'api.wikiwho.net',
@@ -104,8 +115,10 @@ custom_data = {
         {'/rev_content/page_id/{page_id}/':
          # {'get': {'description': '# Some description \n **with** *markdown* \n\n [Markdown Cheatsheet]
          # (https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)',
-             {'get': {'description': 'Outputs the content of the last revision of an article. \n\n'
-                                     'Check `GET /rev_content/{article_title}/` for explanations of query parameters.',
+             {'get': {'description': 'Outputs the content of the most recent (last) revision of the given article, '
+                                     ' as available on Wikipedia.\n\n'
+                                     # 'Outputs the content of the most recent (last) revision of an article. \n\n'
+                                     'This is functionally equivalent to `GET /rev_content/{article_title}/`.',
                       # 'produces': ['application/json'],
                       'parameters': [{'description': 'Page ID of the article from Wikipedia',
                                       'in': 'path',
@@ -131,11 +144,12 @@ custom_data = {
                                      ] + query_params,
                       'responses': responses,
                       'tags': ['1 - Content per revision'],
-                      'summary': 'Get the content of a revision'
+                      'summary': 'Get the content of a specific revision of an article'
                       }
               },
          '/rev_content/{article_title}/':
-             {'get': {'description': 'Outputs the content of the last revision of the given article.\n\n'
+             {'get': {'description': 'Outputs the content of the most recent (last) revision of the given article, '
+                                     ' as available on Wikipedia.\n\n'
                                      '#### Query parameter explanations:\n\n'
                                      '**o_rev_id:** The ID of the revision where the token was added originally '
                                      'in the article.\n\n'
@@ -148,13 +162,13 @@ custom_data = {
                                      '**token_id:** The token ID assigned internally by the WikiWho algorithm, unique '
                                      'per article. Token IDs are assigned increasing from 1 for each new token added '
                                      'to an article.\n\n'
+                                     '**out:** List of all revisions in which the token was deleted, ordered '
+                                     'sequentially by time. If empty, the token has never been deleted.\n\n'
                                      '**in:** List of all revisions where the token was REinserted after '
                                      'being deleted previously, ordered sequentially by time. If empty, the token has '
                                      'never been reintroduced after deletion. Each "in" has to be preceded by one '
-                                     'equivalent "out" in sequence.\n\n'
-                                     '**out:** List of all revisions in which the token was deleted, ordered '
-                                     'sequentially by time. If empty, the token has never been deleted.',
-                      'parameters': [{'description': 'The title of the requested title',
+                                     'equivalent "out" in sequence.',
+                      'parameters': [{'description': 'The title of the requested article',
                                       'in': 'path',
                                       'name': 'article_title',
                                       'required': True,
@@ -168,7 +182,7 @@ custom_data = {
          '/rev_content/{article_title}/{rev_id}/':
              {'get': {'description': 'Outputs the content of the given revision of the given article.\n\n'
                                      'Check `GET /rev_content/{article_title}/` for explanations of query parameters.',
-                      'parameters': [{'description': 'The title of the requested title',
+                      'parameters': [{'description': 'The title of the requested article',
                                       'in': 'path',
                                       'name': 'article_title',
                                       'required': True,
@@ -191,7 +205,7 @@ custom_data = {
                                      'ordinal position, **not** its integer value. These two can very rarely be '
                                      'conflicted.\n\n'
                                      'Check `GET /rev_content/{article_title}/` for explanations of query parameters.',
-                      'parameters': [{'description': 'The title of the requested title',
+                      'parameters': [{'description': 'The title of the requested article',
                                       'in': 'path',
                                       'name': 'article_title',
                                       'required': True,
@@ -213,7 +227,8 @@ custom_data = {
                       }
               },
          '/all_content/page_id/{page_id}/':
-             {'get': {'description': 'Outputs the complete content (all tokens) of the given article.\n\n'
+             {'get': {'description': 'Outputs all tokens that have ever existed in a given article, '
+                                     'including their change history for each.\n\n'
                                      '\n\n'
                                      'Check `GET /all_content/{article_title}/` for explanations of query parameters.',
                       'parameters': [{'description': 'Page ID of the article from Wikipedia',
@@ -239,7 +254,7 @@ custom_data = {
                                      '#### Query parameter explanations:\n\n'
                                      'Query parameters are equivalent to revision content queries. '
                                      'There is only an extra `threshold` parameter.',
-                      'parameters': [{'description': 'The title of the requested title',
+                      'parameters': [{'description': 'The title of the requested article',
                                       'in': 'path',
                                       'name': 'article_title',
                                       'required': True,
@@ -288,7 +303,7 @@ custom_data = {
          #                             'Query parameters are equivalent to revision content queries except that '
          #                             'at least one entry exists in the out list of each token and at least one '
          #                             'more out than in.\n',
-         #              'parameters': [{'description': 'The title of the requested title',
+         #              'parameters': [{'description': 'The title of the requested article',
          #                              'in': 'path',
          #                              'name': 'article_title',
          #                              'required': True,
@@ -335,7 +350,7 @@ custom_data = {
                                      '#### Query parameter explanations:\n\n'
                                      '**editor:** See other query explanations.\n\n'
                                      '**timestamp:** The creation timestamp of the revision as provided by Wikipedia',
-                      'parameters': [{'description': 'The title of the requested title',
+                      'parameters': [{'description': 'The title of the requested article',
                                       'in': 'path',
                                       'name': 'article_title',
                                       'required': True,
