34d5719b7f8f620038a3fb6b6f62ed27b9b62794
Kenan Erdogan <kenanerdogan@gmail.com>
2017-03-31 13:57:56 +0200

    flower config persistent

---
+++ flowerconfig.py
@@ -0,0 +1,12 @@
+# from flower.utils.template import humanize
+
+
+# def format_task(task):
+#     task.args = humanize(task.args, length=10)
+#     task.kwargs.pop('credit_card_number')
+#     task.result = humanize(task.result, length=20)
+#     return task
+
+persistent = True  # defualt False
+max_tasks = 10000  # default 10000
+# tasks_columns = 'name'
