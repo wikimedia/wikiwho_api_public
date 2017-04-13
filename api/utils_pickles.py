115a0c03983a7173520a5001b7c6d2fda1bbe8ba
Kenan Erdogan <kenanerdogan@gmail.com>
2017-04-13 14:13:51 +0200

    try to fix: EOFError Ran out of input

--- api/utils_pickles.py
+++ api/utils_pickles.py
@@ -65,9 +65,20 @@ def pickle_load(pickle_path):
     # try to lock file:
     # 1) if not already locked, then lock, read and then unlock
     # 2) if already locked, wait until unlocked (in timeout) and then lock, read and unlock
-    with OpenFileLock(pickle_path, 'rb', timeout=settings.PICKLE_OPEN_TIMEOUT) as f:
-        obj = pickle.load(f)
-    return obj
+    retries = 3
+    while retries:
+        retries -= 1
+        try:
+            with OpenFileLock(pickle_path, 'rb', timeout=settings.PICKLE_OPEN_TIMEOUT) as f:
+                obj = pickle.load(f)
+            return obj
+        except EOFError:
+            # EOFError Ran out of input
+            # EOFError usually happens when pickle file is empty (size 0)
+            # TODO is this case valid for us? is this True
+            time.sleep(0.1)
+            if not retries:
+                raise
 
 
 def get_pickle_size(page_id):
