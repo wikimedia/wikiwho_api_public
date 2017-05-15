ceb0afa0260f4c4bd9ace5f99b2bbd62cbf8b830
Kenan Erdogan <kenanerdogan@gmail.com>
2017-05-15 15:24:22 +0200

    update

--- wikiwho/tools_dataset/analysis/wiki_learning_curves/aggregate_editors.py
+++ wikiwho/tools_dataset/analysis/wiki_learning_curves/aggregate_editors.py
@@ -11,6 +11,8 @@ from datetime import datetime
 def aggregate_over_editors(partitions, output, header, min_edits, first_edit_year):
     # {'editor': {ts: rev_id, ..}, ..}
     aggregation = defaultdict(dict)
+    first_edit_date = datetime(first_edit_year, 1, 1, tzinfo=pytz.UTC)
+    before_editors = set()
     # Read each partition.
     for partition_file in partitions:
         print("Parsing ", partition_file)
@@ -20,10 +22,18 @@ def aggregate_over_editors(partitions, output, header, min_edits, first_edit_yea
             for line in partition:
                 # editor,edit_no,rev_id,rev_ts,page_id
                 rev_ts = parser.parse(line[3])
-                aggregation[line[0]][rev_ts] = (line[2], line[4])
+                editor = line[0]
+                if rev_ts < first_edit_date:
+                    before_editors.add(editor)
+                    if editor in aggregation:
+                        print('allahu ekber')
+                        del aggregation[editor]
+                elif editor not in before_editors:
+                    aggregation[editor][rev_ts] = line[2]
+                    # aggregation[editor][rev_ts] = (line[2], line[4])
+    del before_editors
 
     # output aggregated editors data
-    first_edit_date = datetime(first_edit_year, 1, 1, tzinfo=pytz.UTC)
     last_date = datetime(2016, 10, 31, tzinfo=pytz.UTC)
     with open(output, 'w') as f_out:
         f_out.write(header)
@@ -32,17 +42,19 @@ def aggregate_over_editors(partitions, output, header, min_edits, first_edit_yea
                 # min 250 edits in total
                 continue
             sorted_timestamps = sorted(data)
-            if sorted_timestamps[0] < first_edit_date:
-                # first edit after 2014
-                continue
+            # if sorted_timestamps[0] < first_edit_date:
+            #     # first edit after 2014
+            #     continue
             weeks = (last_date - sorted_timestamps[0]).days / 7.0
             if len(data) / weeks < 1:
                 # print(len(data), weeks, sorted_timestamps[0])
                 # min 1 edit per week on average
                 continue
             for i, rev_ts in enumerate(sorted_timestamps):
-                rev_id, page_id = data[rev_ts]
+                rev_id = data[rev_ts]
+                # rev_id, page_id = data[rev_ts]
                 f_out.write(editor + ',' + str(i) + ',' + rev_id + ',' + str(rev_ts) + '\n')
+                # f_out.write(editor + ',' + str(i) + ',' + rev_id + ',' + str(rev_ts) + ',' + str(page_id) + '\n')
 
 
 def get_args():
@@ -55,6 +67,7 @@ def get_args():
     parser.add_argument('-e', '--edits', type=int, help='Min # edits that editor should have done. Default is 250')
     # parser.add_argument('-s', '--size', type=int, help='Number of editors that each csv max contains. Default is 100')
     parser.add_argument('-y', '--year', type=int, help='Year that editor first made an edit. Default is 2014')
+    # parser.add_argument('-b', '--batch_size', type=int, help='')
 
     args = parser.parse_args()
 
@@ -69,7 +82,8 @@ def main():
     input_folder = input_folder if input_folder.endswith('/') else input_folder + '/'
     partitions = glob.glob(input_folder + "editors-part*.csv")
     output = input_folder + "editors-all-parts-filtered.csv"
-    header = 'editor,edit_no,rev_id,rev_ts,page_id\n'
+    # header = 'editor,edit_no,rev_id,rev_ts,page_id\n'
+    header = 'editor,edit_no,rev_id,rev_ts\n'
     print("Start: ", strftime("%Y-%m-%d-%H:%M:%S"))
     aggregate_over_editors(partitions, output, header, min_edits, first_edit_year)
     print("End: ", strftime("%Y-%m-%d-%H:%M:%S"))
