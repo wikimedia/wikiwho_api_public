f41928090db5e811459ee72d58511bd974355d73
Kenan Erdogan <kenanerdogan@gmail.com>
2017-09-20 08:56:00 +0200

    postgres backup bash script

---
+++ cron/backup_db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+HOSTNAME="localhost"
+USERNAME="postgres"
+
+function perform_backups()
+{
+    BACKUP_DIR="/home/nuser/db_disk/postgres_backups/ww_api_live/"
+    if ! mkdir -p $BACKUP_DIR; then
+        echo "Cannot create backup directory in $BACKUP_DIR. Go and fix it!" 1>&2
+        exit 1;
+    fi;
+
+    BACKUP_FILE=$BACKUP_DIR"/`date +\%Y-\%m-\%d`-"
+    DB_LIST="ww_api_live"  # comma separated
+    for DATABASE in ${DB_LIST//,/ }
+    do
+        if ! pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $BACKUP_FILE"$DATABASE".sql.gz.in_progress; then
+            echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
+        else
+            rm -f *.sql.gz  # delete old one when new one is created
+            mv $BACKUP_FILE"$DATABASE".sql.gz.in_progress $BACKUP_FILE"$DATABASE".sql.gz  # rename new one
+        fi
+    done
+
+    echo -e "\nAll database backups complete!"
+}
+
+perform_backups
