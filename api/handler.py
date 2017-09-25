a2e958e597e34431d26b0b1fd2b3b5a18c36e2c4
Kenan Erdogan <kenanerdogan@gmail.com>
2017-09-25 09:38:33 +0200

    improve api query handler

--- api/handler.py
+++ api/handler.py
@@ -6,16 +6,13 @@ import os
 import sys
 from requests.exceptions import ConnectionError
 
-# import logging
 # from time import time
-# from builtins import open
 from django.conf import settings
 from django.core.cache import cache
 from django.utils.translation import get_language, get_language_info
 
 from deployment.gunicorn_config import timeout as gunicorn_timeout
 from deployment.celery_config import user_task_soft_time_limit
-from wikiwho.utils_db import wikiwho_to_db
 from wikiwho.wikiwho_simple import Wikiwho
 from .utils import get_latest_revision_data, create_wp_session, Timeout, generate_rvcontinue, get_wp_api_url
 from .utils_pickles import pickle_dump, pickle_load, get_pickle_folder
@@ -40,7 +37,6 @@ class WPHandler(object):
     def __init__(self, article_title, page_id=None, pickle_folder='', save_tables=(),
                  check_exists=True, is_xml=False, revision_id=None, log_error_into_db=True,
                  language=None, *args, **kwargs):
-        # super(WPHandler, self).__init__(article_title, pickle_folder=pickle_folder, *args, **kwargs)
         self.article_title = article_title
         self.saved_article_title = ''
         self.revision_ids = []
@@ -57,13 +53,12 @@ class WPHandler(object):
         self.is_xml = is_xml
         self.namespace = 0
         self.cache_key = None
+        self.cache_set = False
         self.log_error_into_db = log_error_into_db
         self.language = language or get_language()
 
     def __enter__(self):
         # time1 = time()
-        # logging.debug("--------")
-        # logging.debug(self.article_title)
         # check if given page_id valid
         if self.page_id:
             self.page_id = int(self.page_id)
@@ -84,7 +79,6 @@ class WPHandler(object):
             if not settings.TESTING:
                 self.cache_key = 'page_{}_{}'.format(self.language, self.page_id)
 
-        # logging.debug("trying to load pickle")
         pickle_folder = self.pickle_folder or get_pickle_folder(self.language)
         self.pickle_path = "{}/{}.p".format(pickle_folder, self.page_id)
         self.already_exists = os.path.exists(self.pickle_path)
@@ -149,43 +143,36 @@ class WPHandler(object):
         elif self.namespace != 0:
             raise WPHandlerException(MESSAGES['invalid_namespace'][0].format(self.namespace),
                                      MESSAGES['invalid_namespace'][1])
-        self.revision_ids = revision_ids or [self.latest_revision_id]
-
-        if settings.ONLY_READ_ALLOWED:
+        elif settings.ONLY_READ_ALLOWED:
             if self.already_exists:
                 return
             else:
                 raise WPHandlerException(*MESSAGES['only_read_allowed'])
 
-        # holds the last revision id which is saved. 0 for new article
-        rvcontinue = self.saved_rvcontinue
+        self.revision_ids = revision_ids or [self.latest_revision_id]
+        if self.revision_ids[-1] in self.wikiwho.revisions:
+            return
 
-        if self.revision_ids[-1] >= int(rvcontinue.split('|')[-1]):
-            # if given rev_id is bigger than saved one
-            # logging.debug("STARTING NOW")
-            session = create_wp_session()
-            params = {'pageids': self.page_id, 'action': 'query', 'prop': 'revisions',
-                      'rvprop': 'content|ids|timestamp|sha1|comment|flags|user|userid',
-                      'rvlimit': 'max', 'format': 'json', 'continue': '', 'rvdir': 'newer'}
-            # TODO use is_api call? for example celery is not api call! dont forget to do the same for deleting key.
-            if not settings.TESTING:
-                if cache.get(self.cache_key, '0') != '1':
-                    cache.set(self.cache_key, '1', timeout or gunicorn_timeout)
-                else:
-                    raise WPHandlerException(MESSAGES['revision_under_process'][0].format(self.revision_ids[-1],
-                                                                                          self.article_title or self.page_id,
-                                                                                          user_task_soft_time_limit),
-                                             MESSAGES['revision_under_process'][1])
+        # set cache key to prevent processing an article simultaneously
+        if not settings.TESTING:
+            if cache.get(self.cache_key, '0') != '1':
+                cache.set(self.cache_key, '1', timeout or gunicorn_timeout)
+                self.cache_set = True
+            else:
+                raise WPHandlerException(MESSAGES['revision_under_process'][0].format(self.revision_ids[-1],
+                                                                                      self.article_title or self.page_id,
+                                                                                      user_task_soft_time_limit),
+                                         MESSAGES['revision_under_process'][1])
 
-        # TODO: do we need this check in while anymore?
-        while self.revision_ids[-1] >= int(rvcontinue.split('|')[-1]):
-            # FIXME compare timestamps!
+        # process new revisions of the article
+        rvcontinue = self.saved_rvcontinue  # holds the last revision id which is saved. 0 for new article
+        session = create_wp_session()
+        params = {'pageids': self.page_id, 'action': 'query', 'prop': 'revisions',
+                  'rvprop': 'content|ids|timestamp|sha1|comment|flags|user|userid',
+                  'rvlimit': 'max', 'format': 'json', 'continue': '', 'rvdir': 'newer',
+                  'rvendid': self.revision_ids[-1]}
+        while True:
             # continue downloading as long as we reach to the given rev_id limit
-            # if rvcontinue > self.revision_ids[-1], it means this rev_id is saved,
-            # so no calculation is needed
-            # logging.debug('doing partial download')
-            # logging.debug(rvcontinue)
-
             if rvcontinue != '0' and rvcontinue != '1':
                 params['rvcontinue'] = rvcontinue
             try:
@@ -221,8 +208,6 @@ class WPHandler(object):
                     raise WPHandlerException(MESSAGES['article_not_in_wp'][0].format(self.article_title or self.page_id,
                                                                                      get_language_info(self.language)['name'].lower()),
                                              MESSAGES['article_not_in_wp'][1])
-                # elif not pages.get('revision'):
-                #     raise WPHandlerException(message="End revision ID does not exist!")
                 # pass first item in pages dict
                 _, page = result['query']['pages'].popitem()
                 if 'missing' in page:
@@ -252,9 +237,6 @@ class WPHandler(object):
                 break
             rvcontinue = result['continue']['rvcontinue']
             self.wikiwho.rvcontinue = rvcontinue  # used at end to decide if there is new revisions to be saved
-            # print rvcontinue
-
-        # logging.debug('final rvcontinue ' + str(self.wikiwho.rvcontinue))
 
         # time2 = time()
         # print("Execution time handle: {}".format(time2-time1))
@@ -279,10 +261,10 @@ class WPHandler(object):
             # and there is a new revision or first revision of the article
             self.wikiwho.clean_attributes()
             pickle_dump(self.wikiwho, self.pickle_path)
-            if self.save_tables:
-                wikiwho_to_db(self.wikiwho, save_tables=self.save_tables)
-            if not settings.TESTING and not self.is_xml:
-                cache.delete(self.cache_key)
+            # if self.save_tables:
+            #     wikiwho_to_db_task.delay(self.wikiwho, self.language, self.save_tables)
+        if self.cache_set:
+            cache.delete(self.cache_key)
         # return True
         # time2 = time()
         # print("Execution time exit: {}".format(time2-time1))
