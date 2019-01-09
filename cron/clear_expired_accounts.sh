686faee60ddead52f9d750ddc40ab9ad8831e73a
Ulloa, Roberto <roberto.ur@gmail.com>
2019-01-09 14:41:48 +0100

    updating cron tabs to iwwa366

--- cron/clear_expired_accounts.sh
+++ cron/clear_expired_accounts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/home/nuser/venvs/iwwa/bin/python /home/nuser/wikiwho_api/cron/manage.py clear_expired_accounts
+/home/nuser/venvs/iwwa366/bin/python /home/nuser/wikiwho_api/cron/manage.py clear_expired_accounts
 echo 'clear_expired_accounts is finished'
--- cron/clear_sessions.sh
+++ cron/clear_sessions.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/home/nuser/venvs/iwwa/bin/python /home/nuser/wikiwho_api/manage.py clearsessions
+/home/nuser/venvs/iwwa366/bin/python /home/nuser/wikiwho_api/manage.py clearsessions
 echo 'clearsessions is finished'
