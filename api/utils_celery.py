ac158b9022317990fc687db1bde1be96d5b40f23
Ulloa, Roberto <roberto.ur@gmail.com>
2019-01-21 18:26:44 +0100

    fixing messages for production

--- api/utils_celery.py
+++ api/utils_celery.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+import logging
 
 from wikiwho_api.celery import app
 from deployment.celery_config import worker_name_default, worker_name_user
@@ -18,20 +19,26 @@ inspector = app.control.inspect([worker_name_default, worker_name_user])
 # reserved: List of currently reserved tasks, not including scheduled/active.
 # scheduled: List of currently scheduled ETA/countdown tasks.
 
-logger = get_base_logger('events_streamer', settings.EVENTS_STREAM_LOG)
+if setting.DEBUG:
+    logger = get_base_logger('events_streamer', settings.EVENTS_STREAM_LOG, level=logging.DEBUG)
+else:
+    logger = get_base_logger('events_streamer', settings.EVENTS_STREAM_LOG, level=logging.WARNING)
+
+
 
 def get_active_task_pages():
     """Return page titles of tasks that are running right now."""
-    logger.error("Failed to retrieve active pages:\n" + str(e))
-    logger.info("The reserved task will be assume to be empty")
     try:
-        active_tasks = inspector.active() or []  # {'worker_name': [active tasks], 'worker_name2': [..], ..}
+        # {'worker_name': [active tasks], 'worker_name2': [..], ..}
+        active_tasks = inspector.active() or []
     except ConnectionResetError as e:
-        logger.error("The connection has been reset when listing active pages:\n" + str(e))
+        logger.error(
+            "The connection has been reset when listing active pages:\n" + str(e) +
+            "\nThe reserved_tasks will be assume to be empty")
         reserved_tasks = []
     except Exception as e:
-        logger.error("Failed to retrieve active pages:\n" + str(e))
-        logger.info("The reserved task will be assume to be empty")
+        logger.error("Failed to retrieve active pages:\n" + str(e) +
+                     "\nThe reserved_tasks will be assume to be empty")
         reserved_tasks = []
 
     """
@@ -48,18 +55,18 @@ def get_active_task_pages():
 def get_inactive_task_pages():
     """Return page titles of tasks that are registered to Celery. This does not contain tasks in queue."""
     # inspector.scheduled()[worker_name] - we have no scheduled tasks
-    logger.error("Failed to retrieve inactive pages:\n" + str(e))
-    logger.info("The reserved task will be assume to be empty")
-    try: 
-        reserved_tasks = inspector.reserved() or []  # {'worker_name': [reserved tasks]}
+    try:
+        # {'worker_name': [reserved tasks]}
+        reserved_tasks = inspector.reserved() or []
     except ConnectionResetError as e:
-        logger.error("The connection has been reset when listing inactive pages:\n" + str(e))
-        reserved_tasks = [] 
-    except Exception as e:
-        logger.error("Failed to retrieve inactive pages:\n" + str(e))
-        logger.info("The reserved task will be assume to be empty")
+        logger.error(
+            "The connection has been reset when listing inactive pages:\n" + str(e) +
+            "\nThe reserved_tasks will be assume to be empty")
         reserved_tasks = []
-
+    except Exception as e:
+        logger.error("Failed to retrieve inactive pages:\n" + str(e) +
+                     "\nThe reserved_tasks will be assume to be empty")
+        reserved_tasks = []
 
     return [task['args'][2:-3] for worker_name in reserved_tasks for task in reserved_tasks[worker_name] or []]
 
@@ -68,6 +75,7 @@ def process_changed_articles():
     for language, page_title in iter_changed_pages(logger):
         # print(len(get_inactive_task_pages()))
         if page_title not in get_inactive_task_pages():
+            logger.info(f"Processing {page_title} ({language})")
             # if already not registered to celery
             process_article.delay(language, page_title)
             # FIXME event data doesnt contain pageid! decide a limit + settings.PICKLE_BIG_SIZE_LIMIT
