445d675acc0c9a7489961a62735677d318adcc25
Ulloa, Roberto <roberto.ur@gmail.com>
2019-01-24 10:29:25 +0100

    going back to streamer and logger

--- api/utils_celery.py
+++ api/utils_celery.py
@@ -5,7 +5,7 @@ from wikiwho_api.celery import app
 from deployment.celery_config import worker_name_default, worker_name_user
 from django.conf import settings
 
-from base.utils_log import get_base_logger
+from base.utils_log import get_base_logger, get_stream_base_logger
 from .events_stream import iter_changed_pages
 from .tasks import process_article
 # from .utils_pickles import get_pickle_size
@@ -19,12 +19,8 @@ inspector = app.control.inspect([worker_name_default, worker_name_user])
 # reserved: List of currently reserved tasks, not including scheduled/active.
 # scheduled: List of currently scheduled ETA/countdown tasks.
 
-if settings.DEBUG:
-    logger = get_base_logger('events_streamer', settings.EVENTS_STREAM_LOG, level=logging.DEBUG)
-else:
-    logger = get_base_logger('events_streamer', settings.EVENTS_STREAM_LOG, level=logging.DEBUG)
-
-
+logger = get_base_logger('events_streamer', settings.EVENTS_STREAM_LOG, level=logging.WARNING)
+streamer = get_stream_base_logger('streamer', level=logging.DEBUG)
 
 def get_active_task_pages():
     """Return page titles of tasks that are running right now."""
@@ -40,15 +36,6 @@ def get_active_task_pages():
         logger.error("Failed to retrieve active pages:\n" + str(e) +
                      "\nThe reserved_tasks will be assume to be empty")
         reserved_tasks = []
-
-    """
-    task:
-    [{'worker1.example.com':
-    [{'name': 'tasks.sleeptask',
-      'id': '32666e9b-809c-41fa-8e93-5ae0c80afbbf',
-      'args': '(8/"title",)',
-      'kwargs': '{}'}]}]
-    """
     return [task['args'][2:-3] for worker_name in active_tasks for task in active_tasks[worker_name] or []]
 
 
@@ -75,11 +62,6 @@ def process_changed_articles():
     for language, page_title in iter_changed_pages(logger):
         # print(len(get_inactive_task_pages()))
         if page_title not in get_inactive_task_pages():
+            streamer.info(f"EVENT: {page_title} ({language})")
             # if already not registered to celery
             process_article.delay(language, page_title)
-            logger.info(f"Processed {page_title} ({language})")
-            # FIXME event data doesnt contain pageid! decide a limit + settings.PICKLE_BIG_SIZE_LIMIT
-            # if get_pickle_size(page_id) > 446197:
-            #     process_big_sized_article.delay(page_title)
-            # else:
-            #     process_article.delay(page_title)
