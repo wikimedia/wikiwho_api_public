1eb541af406fdd08f01e578d2a672e5ddeb9654c
Ulloa, Roberto <roberto.ur@gmail.com>
2019-04-09 12:09:24 +0200

    redirecting api_xtra to static

--- wikiwho_api/urls.py
+++ wikiwho_api/urls.py
@@ -26,6 +26,13 @@ from base.views import clear_cache, clear_sessions, download, home
 from base.sitemaps import BaseStaticViewSitemap, ApiStaticViewSitemap
 from api.views import ApiRedirectView
 
+from django.shortcuts import redirect
+
+
+def redirect_view(request):
+    return redirect(request.path_info.replace('api_xtra', 'static/images'))
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^admin/clear_cache', clear_cache, name='clear_cache'),
@@ -38,15 +45,21 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'api': ApiStaticViewSitemap, 'base': BaseStaticViewSitemap}},
         name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow:", content_type="text/plain")),
+    url('^api_xtra/', redirect_view),
+    url(r'^robots.txt$', lambda r: HttpResponse(
+        "User-agent: *\nDisallow:", content_type="text/plain")),
 ]
 
 urlpatterns += i18n_patterns(
-    url(r'^api/(?P<version>(v1.0.0-beta|v1.0.0))/', include('api.urls', namespace='api')),
+    url(r'^api/(?P<version>(v1.0.0-beta|v1.0.0))/',
+        include('api.urls', namespace='api')),
     url(r'^api/', ApiRedirectView.as_view()),
-    url(r'^whocolor/(?P<version>(v1.0.0-beta|v1.0.0))/', include('whocolor.urls', namespace='whocolor')),
-    url(r'^edit_persistence/(?P<version>(v1.0.0-beta|v1.0.0))/', include('api_editor.urls', namespace='edit_persistence')),
-    url(r'^api_editor/(?P<version>(v1.0.0-beta|v1.0.0))/', include('api_editor.urls', namespace='api_editor')),
+    url(r'^whocolor/(?P<version>(v1.0.0-beta|v1.0.0))/',
+        include('whocolor.urls', namespace='whocolor')),
+    url(r'^edit_persistence/(?P<version>(v1.0.0-beta|v1.0.0))/',
+        include('api_editor.urls', namespace='edit_persistence')),
+    url(r'^api_editor/(?P<version>(v1.0.0-beta|v1.0.0))/',
+        include('api_editor.urls', namespace='api_editor')),
     # prefix_default_language=False
 )
 
