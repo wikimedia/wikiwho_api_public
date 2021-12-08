36f052440391501497dc9a3a476e7752f63ece2e
Sage Ross <ragesoss@gmail.com>
2021-12-08 15:58:03 -0800

    [WIP] Extend setup documentation and script
    I was able to get through enabling the gunicorn service on Wikimedia Cloud, although I did all the things I've added to the customization script manually so far and haven't tested it via script.
    
    I haven't tested this nginx config yet; it's based on the one from deployment/nginx.conf, but it cuts out all the SSL config since that gets handled by the toolforge web proxy.

--- WIKIMEDIA_VPS_SETUP.md
+++ WIKIMEDIA_VPS_SETUP.md
@@ -10,11 +10,28 @@
    2. From home: `scp wikiwho_api.zip ragesoss@wikiwho-api:/home/ragesoss`
    3. On the VPS: `sudo mv wikiwho_api.zip /home/wikiwho`
 
-4. Become the wikiwho user: `sudo su wikiwho`
-5. `cd /home/wikiwho`
-6. `unzip wikiwho_api.zip`
-7. `cd wikiwho_api`
-8. Set up a virtualenv:
-   1. `python3 -m venv env`
+4. Create the wikiwho postgres user and database:
+   1. `sudo su postgres`
+   2. `psql`
+      1. `create user wikiwho with password 'wikiwho';`
+      2. `create database wikiwho;`
+      3. `grant all privileges on database wikiwho to wikiwho;`
+      4. `exit;`
+   3. `exit`
+5. Become the wikiwho user: `sudo su wikiwho`
+6. `cd /home/wikiwho`
+7. `unzip wikiwho_api.zip`
+8. Create the directory structure for the pickles and dumps:
+9.  `cd wikiwho_api`
+10.  Set up a virtualenv:
+   1. `python -m venv env`
    2. `. env/bin/activate`
-9. Install the Python dependencies: `pip3 install -r requirements.txt -r requirements_local.txt -r requirements_test.txt`
+11.  Install the Python dependencies: `pip install -r requirements.txt -r requirements_local.txt -r requirements_test.txt`
+12.  Generate a secret key: `python manage.py generate_secret_key`
+13.  Create `wikiwho_api/settings.py` (in the wikiwho_api subdirectory, not the top git directory), with an import from one of the environment-specific settings files plus SECRET_KEY, WP_CONSUMER_TOKEN, WP_CONSUMER_SECRET, WP_ACCESS_TOKEN, WP_ACCESS_SECRET, and DATABASES (at least).
+14. `python manage.py migrate`
+15. `python manage.py collectstatic --noinput -c`
+16. As a user with sudo, start the Gunicorn webserver:
+    1.  `sudo systemctl enable ww_gunicorn`
+    2.  `sudo systemctl start ww_gunicorn`
+    3.  `sudo systemctl status ww_gunicorn` to check if it's running
--- wikimedia_cloud_customization_script.sh
+++ wikimedia_cloud_customization_script.sh
@@ -8,6 +8,58 @@ useradd -m wikiwho
 # Set default shell
 usermod --shell /bin/bash wikiwho
 
-# Install prerequisites
-apt-get install -y python3-dev graphviz libgraphviz-dev pkg-config python3-venv postgresql libpq-dev libxml2-dev libxslt-dev virtualenvwrapper
+# Set up the directory structure
+mkdir -p /home/wikiwho/p_disk/{en,eu,es,de,tr}
+mkdir /home/wikiwho/dumps
+chown -R wikiwho /home/wikiwho
 
+mkdir /var/log/django
+chown wikiwho /var/log/django
+
+# Install Python prerequisites
+apt-get install -y python3-dev graphviz libgraphviz-dev pkg-config python3-venv postgresql libpq-dev libxml2-dev libxslt-dev virtualenvwrapper
+# Install production webservice prerequisites
+apt-get install -y nginx memcached libcache-memcached-perl libanyevent-perl
+
+# Per wiki: Change the swappiness to a lower value to better use of RAM memory and not swap
+sysctl -w vm.swappiness=5
+
+# Add gunicorn service
+echo """
+[Unit]
+Description=gunicorn daemon for wikiwho api
+After=network.target
+
+[Service]
+User=wikiwho
+Group=www-data
+WorkingDirectory=/home/wikiwho/wikiwho_api
+ExecStart=/home/wikiwho/wikiwho_api/env/bin/gunicorn --config /home/wikiwho/wikiwho_api/deployment/gunicorn_config.py wikiwho_>
+
+[Install]
+WantedBy=multi-user.target
+""" > /etc/systemd/system/ww_gunicorn.service
+
+echo """
+server {
+    listen 80;
+    server_name wikiwho.toolforge.org;
+
+    add_header X-Frame-Options DENY;
+    add_header X-Content-Type-Options nosniff;
+
+    access_log off;  # turn off access log
+    root /home/wikiwho/wikiwho_api;
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static {
+            root /home/wikiwho/wikiwho_api;
+    }
+
+    location / {
+            include proxy_params;
+            proxy_pass http://unix:/home/wikiwho/wikiwho_api/ww_api_gunicorn.sock;
+            proxy_read_timeout 360s;  # default is 60s
+    }
+}
+""" > /etc/nginx/sites-available/wikiwho
