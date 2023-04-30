6927bd51b8e85070cb6a763d83523f3be1160a3f
MusikAnimal <musikanimal@gmail.com>
2023-04-29 22:38:22 -0400

    event_streams: add huwiki to db list
    Bug: T296590

--- api/events_stream.py
+++ api/events_stream.py
@@ -50,7 +50,7 @@ def stream_response_with_requests(url):
 
 
 def iter_changed_pages(logger):
-    wikis = ['enwiki', 'euwiki', 'dewiki', 'trwiki', 'eswiki', 'frwiki', 'itwiki']
+    wikis = ['enwiki', 'euwiki', 'dewiki', 'trwiki', 'eswiki', 'frwiki', 'itwiki', 'huwiki']
     while True:
         try:
             url = 'https://stream.wikimedia.org/v2/stream/recentchange'
