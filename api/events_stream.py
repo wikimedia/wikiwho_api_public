c3b2546a6b62b771ddfdc8d82b8f5f6e224503bc
MusikAnimal <musikanimal@gmail.com>
2023-05-02 10:25:24 -0400

    event_streams: add jawiki to db list
    Bug: T335599

--- api/events_stream.py
+++ api/events_stream.py
@@ -50,7 +50,7 @@ def stream_response_with_requests(url):
 
 
 def iter_changed_pages(logger):
-    wikis = ['enwiki', 'euwiki', 'dewiki', 'trwiki', 'eswiki', 'frwiki', 'itwiki', 'huwiki']
+    wikis = ['enwiki', 'euwiki', 'dewiki', 'trwiki', 'eswiki', 'frwiki', 'itwiki', 'huwiki', 'jawiki']
     while True:
         try:
             url = 'https://stream.wikimedia.org/v2/stream/recentchange'
