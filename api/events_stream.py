9a2f6fbc9eecb28b1474584fb2c6676d2c8d42de
MusikAnimal <musikanimal@gmail.com>
2023-05-03 11:57:16 -0400

    event_streams: add ptwiki to db list
    Bug: T335600

--- api/events_stream.py
+++ api/events_stream.py
@@ -50,7 +50,7 @@ def stream_response_with_requests(url):
 
 
 def iter_changed_pages(logger):
-    wikis = ['enwiki', 'euwiki', 'dewiki', 'trwiki', 'eswiki', 'frwiki', 'itwiki', 'huwiki', 'jawiki', 'idwiki']
+    wikis = ['enwiki', 'euwiki', 'dewiki', 'trwiki', 'eswiki', 'frwiki', 'itwiki', 'huwiki', 'jawiki', 'idwiki', 'ptwiki']
     while True:
         try:
             url = 'https://stream.wikimedia.org/v2/stream/recentchange'
